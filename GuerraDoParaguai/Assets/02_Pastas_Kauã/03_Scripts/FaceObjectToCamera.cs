f4eb64f89482df3f0f2a5afc59e80590b71dab5f
LuxsKV <100307253+LuxsKV@users.noreply.github.com>
2023-12-06 10:52:12 -0300

    times 2.0??

--- GuerraDoParaguai/Assets/02_Pastas_Kauã/03_Scripts/FaceObjectToCamera.cs
+++ GuerraDoParaguai/Assets/02_Pastas_Kauã/03_Scripts/FaceObjectToCamera.cs
@@ -13,6 +13,9 @@ public class FaceObjectToCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Camera.main.transform);
+        if (Camera.main != null)
+        {
+            transform.LookAt(Camera.main.transform);
+        }
     }
 }
